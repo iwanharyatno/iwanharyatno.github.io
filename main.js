98ed69dae59ced44782a6389b6a881a291ce73fe
iwanharyatno <iwnharry61@gmail.com>
2021-12-13 18:52:05 +0700

    Membuat halaman portfolio

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-function main() {}
+window.onscroll = function() {
+    const navTitle = document.querySelector("nav h2");
+    const nav = document.querySelector("header");
+    const navHeight = window.getComputedStyle(nav).height.replace("px", "") / 1.5;
 
-
-window.onload = main;
+    if (document.documentElement.scrollTop > navHeight || document.body.scrollTop > navHeight) {
+        navTitle.textContent = "Iwan Haryatno";
+    } else {
+        navTitle.textContent = "Portfolio";
+    }
+}
